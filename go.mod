60891f2c0d3b9774795523692c8b560a17a400be
Hombi <philipp.homberger@gmail.com>
2023-03-28 17:53:41 +0200

    add stuff

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ecosystems.schwarz/helloyaml
+
+go 1.16
+
+require gopkg.in/yaml.v3 v3.0.1
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Name string `yaml:"name"`
+	Age  int    `yaml:"age"`
+}
+
+func main() {
+	// Read the file
+	data, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Create a struct to hold the YAML data
+	var config Config
+
+	// Unmarshal the YAML data into the struct
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the data
+	fmt.Println(config.Name)
+}
